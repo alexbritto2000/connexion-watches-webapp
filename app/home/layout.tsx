2034956e28c59c90483f6debf792670391fe3bab
alexbritto-dotcod <alexbritto@dotcod.in>
2025-08-31 22:47:14 +0530

    For home screen powered by scalyx removed

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -21,7 +21,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
             <main>{children}</main>
 
-            <Footer showPoweredBy={true} />
+            <Footer showPoweredBy={false} />
         </div>
     );
 }
